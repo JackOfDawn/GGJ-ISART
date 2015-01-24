6f906f1f8339532ab1d168f7676fad66c13d6dce
Stevy <chassard.s@gmail.com>
2015-01-23 22:56:12 -0500

    qq

---
+++ Jam/Assets/ObjetScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjetScript : MonoBehaviour {
+
+	private bool detected = false;
+	public int ID;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (detected == true) {
+			gameObject.renderer.material.color = new Color (1, 0, 0, 1); // Just a FeedBack that we can use this object
+			if (Input.GetKey (KeyCode.Space)) {
+				Destroy(gameObject); // Put choice of what action we wanna do
+				GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().objectID = ID;
+			}
+		} else {
+			gameObject.renderer.material.color = new Color (1, 1, 1, 1); // Just remove the FeedBack
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D Detection){
+		if (Detection.gameObject.tag == "PlayerDetection"){
+			detected = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D Detection){
+		if (Detection.gameObject.tag == "PlayerDetection"){
+			detected = false;
+		}
+	}
+}
---
+++ Jam/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float vitesse = 5.0f;
+	public int objectID;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector3.right * vitesse * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (Vector3.left * vitesse * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (Vector3.up * vitesse * Time.deltaTime);
+		}
+
+	}
+}
