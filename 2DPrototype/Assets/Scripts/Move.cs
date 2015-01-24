c57baa4212b35e1bde4d28be824ec336e5207b49
Jack Storm <jsto37@gmail.com>
2015-01-23 22:30:41 -0500

    Bonjour. 2D Prototype

---
+++ 2DPrototype/Assets/Scripts/Move.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public int speed = 10;
+	public int jumpPower = 50;
+	public bool isFacingRight = true;
+	public bool grounded;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float value = Input.GetAxis ("Horizontal");
+
+		if(value != 0)	{
+			rigidbody2D.AddForce (Vector2.right * value * speed);
+		}
+
+		RaycastHit2D rayGrounded = Physics2D.Linecast ((Vector2) transform.position, (Vector2)transform.position - Vector2.up);
+
+		if(grounded && Input.GetAxis("Jump") == 1)
+			this.rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, (float)jumpPower);
+		
+
+	}
+
+	void OnTriggerEnter2D() {
+		grounded = true;
+	}
+	void OnTriggerStay2D() {
+		grounded = true;
+	}
+	void OnTriggerExit2D() {
+		grounded = false;
+	}
+}
