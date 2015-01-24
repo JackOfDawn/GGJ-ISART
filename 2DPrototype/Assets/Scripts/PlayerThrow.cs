88fdd4b695cddc390dc3226a8335063011a9e8ee
Jack Storm <jsto37@gmail.com>
2015-01-24 01:52:07 -0500

    WOOOOO

---
+++ 2DPrototype/Assets/Scripts/PlayerThrow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerThrow : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject playerRef;
+	public GameObject throwable;
+	public float MAX_COOLDOWN = 1;
+	private float cooldown;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cooldown -= Time.deltaTime;
+		//if throw button is press, create throwable with velocity in player's direction?
+		if (Input.GetAxis ("Fire1") == 1  && cooldown < 0) {
+			CreateThrowable();
+			cooldown = MAX_COOLDOWN;
+		}
+	}
+
+	void CreateThrowable()	{
+		Instantiate (throwable, transform.position, Quaternion.identity);
+		ThrowableScript shot = throwable.GetComponent<ThrowableScript>();
+		if (shot) {
+			Move playerMove = playerRef.GetComponent<Move>();
+			if(playerMove)	{
+				if(playerMove.isFacingRight)
+					shot.direction = 1;
+				else
+					shot.direction = -1;
+			}
+		}
+	}
+}
---
+++ 2DPrototype/Assets/Scripts/ThrowableScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowableScript : MonoBehaviour {
+
+	// Use this for initialization
+	public int speed = 5;
+	public float activeSeconds = 3;
+	public int direction = 1; // 1 = right, -1 = left
+	private float remainingSeconds;
+	void Start () {
+		remainingSeconds = activeSeconds;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 velocity = rigidbody2D.velocity;
+		velocity.x = speed * direction;// * Time.deltaTime;
+		rigidbody2D.velocity = velocity;
+
+		remainingSeconds -= Time.deltaTime;
+		if (remainingSeconds <= 0) {
+			Destroy(this.gameObject);
+		}
+	}
+
+	public void setDirection()
+	{
+
+	}
+}
